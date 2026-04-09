637f3022b559ba37afcb2e3d4493dbd7223d6ce0
Mauro F. <maforeror@gmail.com>
2026-04-08 21:49:58 -0500

    basic timerservice

--- PomodoroTimer/PomodoroTimer/Services/TimerService.cs
+++ PomodoroTimer/PomodoroTimer/Services/TimerService.cs
@@ -2,5 +2,26 @@
 {
     public class TimerService
     {
+        private CancellationTokenSource? cts;
+
+        public async Task StartAsync() 
+        {
+
+        }
+
+        public async Task RestartAsync()
+        {
+
+        }
+
+        public async Task AddTimeAsync()
+        {
+
+        }
+
+        public async Task CancelAsync()
+        {
+
+        }
     }
 }
