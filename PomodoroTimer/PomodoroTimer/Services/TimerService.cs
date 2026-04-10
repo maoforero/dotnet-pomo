fb8a32240d07f56022a470f798695579f2710f40
Mauro F. <maforeror@gmail.com>
2026-04-09 22:26:11 -0500

    service timer

--- PomodoroTimer/PomodoroTimer/Services/TimerService.cs
+++ PomodoroTimer/PomodoroTimer/Services/TimerService.cs
@@ -1,25 +1,39 @@
-﻿using PomodoroTimer.Models;
+﻿using System.Threading.Tasks;
+using PomodoroTimer.Models;
 
 namespace PomodoroTimer.Services
 {
     public class TimerService
     {
-        private CancellationTokenSource? _cts;
-        private  PomodoroSession? _session;
+        private CancellationTokenSource?   _cts;
+        private PomodoroSession?           _session;
 
         public async Task StartAsync(int seconds, bool alarmState) 
         {
             _session = new PomodoroSession(seconds, alarmState);
+            _cts = new CancellationTokenSource();
+
+            while (!_cts.Token.IsCancellationRequested)
+            {
+                await Task.Delay(TimeSpan.FromSeconds(1), _cts.Token);
+                var now = DateTime.Now;
+
+                if(now >= _session.TimeLimit)
+                {
+                    // Proceso de alarma en la UI
+                    _cts.Cancel();
+                }
+                
+            }
         }
 
-        public void Restart(int seconds)
+        public async Task Restart(int seconds)
         {
             ArgumentNullException.ThrowIfNull(_session);
             ArgumentNullException.ThrowIfNull(_cts);
+            var alarmState = _session.AlarmIsActive;
             _cts.Cancel();
-            _session.RestartSession(seconds);
-            _session = new PomodoroSession(seconds, true);
-
+            await StartAsync(seconds, alarmState);
         }
 
         public void Cancel()
