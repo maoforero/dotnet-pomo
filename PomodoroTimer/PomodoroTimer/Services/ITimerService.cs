ca0e942464f8292202acb19104bc8c4f2c9dc383
Mauro F. <maforeror@gmail.com>
2026-04-12 23:12:12 -0500

    ITimserService interface added

---
+++ PomodoroTimer/PomodoroTimer/Services/ITimerService.cs
@@ -0,0 +1,15 @@
+﻿namespace PomodoroTimer.Services
+{
+    public interface ITimerService
+    {
+        Task StartAsync(int seconds, bool alarmState);
+        Task Restart(int seconds);
+        void Cancel();
+        void AddTime(int seconds);
+        void DisableAlarm();
+        TimeSpan Remaining();
+        bool AlarmIsActive();
+        int TotalSeconds();
+        bool IsCompleted();
+    }
+}
